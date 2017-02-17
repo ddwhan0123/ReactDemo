d9218e7e00e305a765c2ea021b3dcdd6eb237a45
ddwhan0123 <526357367@qq.com>
2017-02-17 17:11:38 +0800

    格式刷

--- app/main.js
+++ app/main.js
@@ -4,6 +4,7 @@ import { Router , Route , hashHistory } from 'react-router';
 import first from './first';
 import two from './two';
 import three from './three';
+
 render((
   <Router history={hashHistory}>
     <Route path="/" component={first}>
